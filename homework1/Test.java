cd66ad6f922a50b0b5a96e7c08f59b6a3742ec16
Alun <39848824+Dillon595@users.noreply.github.com>
2021-01-08 16:06:47 +0800

    Update Test.java

--- homework1/Test.java
+++ homework1/Test.java
@@ -72,7 +72,7 @@ public class Test {
 		for(int i=0;i<list2.size()-1;i++) {
 			for(int j=0;j<list2.size()-1-i;j++) {
 				int x = ((String) list2.get(j)).charAt(0);  //拿到集合中相邻位置字符串的第一个字符串中的第一个字符（char型用ASCII编码，所以直接用int接收）
-				int y = ((String) list2.get(j+1)).charAt(0);//拿到集合中相邻位置字符串的第二个字符串中的第一个字符  例如第一次循环时拿到（a,ab,abc）中ab的第一个字符a
+				int y = ((String) list2.get(j+1)).charAt(0);//拿到集合中相邻位置字符串的第二个字符串中的第一个字符  例如第一次循环时拿到（a,ab,abc）中ab的第一个字符a的ASCII编码值97
 				if(x>y) {					//如果第一个字符串的字符大于第二个字符串的字符
 					String temp = (String) list2.set(j, list2.get(j+1));	//list.set(j, "asd")方法会返回第j个位置未被修改时的数据
 					list2.set(j+1, temp);
