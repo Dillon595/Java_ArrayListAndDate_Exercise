947da02db8a57048f4c5b4b25237dfd983f94ae9
Alun <39848824+Dillon595@users.noreply.github.com>
2021-01-08 16:05:13 +0800

    Update Test.java

--- homework1/Test.java
+++ homework1/Test.java
@@ -67,19 +67,19 @@ public class Test {
 		
 		System.out.println("**********************************");
 		System.out.println("字母顺序降序排列为：");
-		ArrayList list2 = (ArrayList) list.clone();
+		ArrayList list2 = (ArrayList) list.clone();			//复制一个新的集合，以避免更改原来的集合
 		
 		for(int i=0;i<list2.size()-1;i++) {
 			for(int j=0;j<list2.size()-1-i;j++) {
-				int x = ((String) list2.get(j)).charAt(0);
-				int y = ((String) list2.get(j+1)).charAt(0);
-				if(x>y) {
-					String temp = (String) list2.set(j, list2.get(j+1));
+				int x = ((String) list2.get(j)).charAt(0);  //拿到集合中相邻位置字符串的第一个字符串中的第一个字符（char型用ASCII编码，所以直接用int接收）
+				int y = ((String) list2.get(j+1)).charAt(0);//拿到集合中相邻位置字符串的第二个字符串中的第一个字符  例如第一次循环时拿到（a,ab,abc）中ab的第一个字符a
+				if(x>y) {					//如果第一个字符串的字符大于第二个字符串的字符
+					String temp = (String) list2.set(j, list2.get(j+1));	//list.set(j, "asd")方法会返回第j个位置未被修改时的数据
 					list2.set(j+1, temp);
 				}
 			}
 		}
-		for(int i=list2.size()-1;i>=0;i--) {
+		for(int i=list2.size()-1;i>=0;i--) {				//循环输出集合中的元素
 			System.out.println(list2.get(i));
 			
 		}
